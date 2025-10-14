860c034ad32e75568f738ed2574599bc9a581224
Hernan Brosio <hernan.brosio@gmail.com>
2025-10-13 21:26:56 -0300

    nuevos cambios en los menus y peocedimientos

--- sitemaFacturacion/sitemaFacturacion.cpp
+++ sitemaFacturacion/sitemaFacturacion.cpp
@@ -7,8 +7,6 @@ struct Cliente
     string nombre;
     string apellido;
     string email;
-    string direccion;
-    int telefono;
 };
 
 struct Factura
@@ -18,144 +16,279 @@ struct Factura
     int facturaAnio;
     string numeroFactura;
     int clienteID;
-    string productoSKU;
+    int productoSKU[10];
     string productoNombre;
     int productoCantidad;
     float precioUnitario;
     float precioTotal;
-    float descuento;
-    float IVA;
     float facturaTotal;
 };
 
 struct Producto
 {
-    string productoSKU;
+    int productoSKU;
     string productoNombre;
-    string productoDescripcion;
-    int productoPrecio;
+    float productoPrecio;
     int productoStock;
 };
 
-void menuPrincipal(int opcion1);
-void menuClietes(int opcion1);
-void menuProductos(int opcion1);
-void menuFacturas(int opcion1);
+void menuPrincipal();
+void menuClientes(Cliente clientes[], int &cantidadClientes);
+void menuFacturas(Factura facturas[], int &cantidadFacturas, Cliente clientes[], int cantidadClientes, Producto productos[], int cantidadProductos);
+void menuCierreCaja();
+void agregarCliente(Cliente clientes[], int &cantidadClientes);
+void listarCliente(Cliente clientes[], int cantidadClientes);
+void inventario(Producto productos[], int cantidadProductos);
+void ingresarFactura(Factura facturas[], int &cantidadFacturas, Cliente clientes[], int cantidadClientes, Producto productos[], int cantidadProductos);
+void listarFacturas(Factura facturas[], int cantidadFacturas, Cliente clientes[], int cantidadClientes, Producto productos[], int cantidadProductos);
+void cierreCaja(Factura facturas[], int cantidadFacturas, Cliente clientes[], int cantidadClientes, Producto productos[], int cantidadProductos);
 
 int main()
 {
-    int opcion1;
-    menuPrincipal(opcion1);
+
+    menuPrincipal();
 }
-void menuPrincipal(int opcion1)
+void menuPrincipal()
 {
-    int opcion;
-    cout << "---SISTEMA DE FACTURACION---" << endl;
-    cout << "--------MENU PRINCIPA--------" << endl;
-    cout << "Elija la opcion deseada: " << endl;
-    cout << "1. Gestion de Clientes" << endl;
-    cout << "2. Gestion de Productos" << endl;
-    cout << "3. Gestion de Facturas" << endl;
-    cout << "4. Salir" << endl;
-    cout << "-----------------------------" << endl;
-    cin >> opcion;
+    static Cliente clientes[100]{};
+    static Producto productos[10]{};
+    static Factura facturas[100]{};
 
-    if (opcion < 1 || opcion > 4)
-    {
-        cout << "Opcion no valida" << endl;
-        menuPrincipal(opcion);
-    }
-    switch (opcion)
-    {
-    case 1:
-        menuClietes(opcion);
-        break;
-    case 2:
-        menuProductos(opcion);
-        break;
-    case 3:
-        menuFacturas(opcion);
-        break;
-    }
-}
+    static int cantidadClientes = 0;
+    static int cantidadProductos = 0;
+    static int cantidadFacturas = 0;
 
-void menuClietes(int opcion1)
-{
-    int opcion;
-    cout << "---GESTION DE CLIENTES---" << endl;
-    cout << "Elija la opcion deseada: " << endl;
-    cout << "1. Agregar Cliente" << endl;
-    cout << "2. Modificar Cliente" << endl;
-    cout << "3. Buscar Cliente" << endl;
-    cout << "4. Listar Clientes" << endl;
-    cout << "5. volver al menu anterior" << endl;
-    cout << "-----------------------------" << endl;
-    cin >> opcion;
-
-    if (opcion < 1 || opcion > 5)
+    while (true)
     {
-        cout << "Opcion no valida" << endl;
-        menuClietes(opcion);
-    }
 
-    if (opcion == 5)
-    {
-        cout << "Opcion no valida" << endl;
-        menuPrincipal(opcion);
+        int opcion;
+        cout << "----SISTEMA DE FACTURACION----" << endl;
+        cout << "--------MENU PRINCIPAL--------" << endl;
+        cout << "Elija la opcion deseada: " << endl;
+        cout << "1. Gestion de Clientes" << endl;
+        cout << "2. Gestion de Facturas" << endl;
+        cout << "3. Salir" << endl;
+        cout << "-----------------------------" << endl;
+        cin >> opcion;
+
+        if (opcion < 1 || opcion > 3)
+        {
+            cout << "Opcion no valida" << endl;
+            continue;
+        }
+        switch (opcion)
+        {
+        case 1:
+            menuClientes(clientes, cantidadClientes);
+            break;
+        case 2:
+            menuFacturas(facturas, cantidadFacturas, clientes, cantidadClientes, productos, cantidadProductos);
+            break;
+        case 3:
+            cout << "Saliendo del sistema..." << endl;
+            return;
+        }
     }
 }
 
-void menuProductos(int opcion1)
+void menuClientes(Cliente clientes[], int &cantidadClientes)
 {
-    int opcion;
-    cout << "---GESTION DE PRODUCTOS---" << endl;
-    cout << "Elija la opcion deseada: " << endl;
-    cout << "1. Agregar Producto" << endl;
-    cout << "2. Modificar Producto" << endl;
-    cout << "3. Buscar Producto" << endl;
-    cout << "4. Inventario" << endl;
-    cout << "5. volver al menu anterior" << endl;
-    cout << "-----------------------------" << endl;
-    cin >> opcion;
-
-    if (opcion < 1 || opcion > 5)
+    while (true)
     {
-        cout << "Opcion no valida" << endl;
-        menuProductos(opcion);
-    }
 
-    if (opcion == 5)
-    {
-        cout << "Opcion no valida" << endl;
-        menuPrincipal(opcion);
+        int opcion;
+        cout << "----GESTION DE CLIENTES----" << endl;
+        cout << "Elija la opcion deseada: " << endl;
+        cout << "1. Agregar Cliente" << endl;
+        cout << "2. Listar Clientes" << endl;
+        cout << "3. volver al menu anterior" << endl;
+        cout << "-----------------------------" << endl;
+        cin >> opcion;
+
+        if (opcion < 1 || opcion > 3)
+        {
+            cout << "Opcion no valida" << endl;
+            continue;
+        }
+
+        if (opcion == 3)
+        {
+            return;
+        }
+
+        switch (opcion)
+        {
+        case 1:
+            agregarCliente(clientes, cantidadClientes);
+            break;
+        case 2:
+            listarCliente(clientes, cantidadClientes);
+            break;
+        }
     }
 }
 
-void menuFacturas(int opcion1)
+
+void menuFacturas(Factura facturas[], int &cantidadFacturas, Cliente clientes[], int cantidadClientes, Producto productos[], int cantidadProductos)
 {
-    int opcion;
-    cout << "---GESTION DE FACTURAS---" << endl;
-    cout << "Elija la opcion deseada: " << endl;
-    cout << "1. Ingresar Factura" << endl;
-    cout << "2. Modificar Factura" << endl;
-    cout << "3. Anular Factura" << endl;
-    cout << "4. Buscar Factura" << endl;
-    cout << "5. Listar Facturas" << endl;
-    cout << "6. Cierre de caja" << endl;
-    cout << "7. volver al menu anterior" << endl;
-    cout << "-----------------------------" << endl;
-    cin >> opcion;
-
-    if (opcion < 1 || opcion > 7)
+    while (true)
     {
-        cout << "Opcion no valida" << endl;
-        menuFacturas(opcion);
-    }
 
-    if (opcion == 7)
+        int opcion;
+        cout << "----GESTION DE FACTURAS----" << endl;
+        cout << "Elija la opcion deseada: " << endl;
+        cout << "1. Ingresar Factura" << endl;
+        cout << "2. Listar Facturas" << endl;
+        cout << "3. Cierre de caja" << endl;
+        cout << "4. volver al menu anterior" << endl;
+        cout << "-----------------------------" << endl;
+        cin >> opcion;
+
+        if (opcion < 1 || opcion > 4)
+        {
+            cout << "Opcion no valida" << endl;
+            continue;
+        }
+
+        if (opcion == 4)
+        {
+            return;
+        }
+        switch (opcion)
+        {
+        case 1:
+            ingresarFactura(facturas, cantidadFacturas, clientes, cantidadClientes, productos, cantidadProductos);
+            break;
+        case 2:
+            listarFacturas(facturas, cantidadFacturas, clientes, cantidadClientes, productos, cantidadProductos);
+            break;
+        case 3:
+            menuCierreCaja();
+            break;
+        }
+    }
+}
+
+void menuCierreCaja()
+{
+    while (true)
     {
-        cout << "Opcion no valida" << endl;
-        menuPrincipal(opcion);
-    }
+        int opcion;
+        cout << "----CIERRE DE CAJA----" << endl;
+        cout << "Elija la opcion deseada: " << endl;
+        cout << "1. Total de venta de la sesion" << endl;
+        cout << "2. Factura de mayor importe" << endl;
+        cout << "3. Promedio de facturacion por factura" << endl;
+        cout << "4. Promedio de productos por factura" << endl;
+        cout << "5. volver al menu anterior" << endl;
+        cout << "-----------------------------" << endl;
+        cin >> opcion;
 
+        if (opcion < 1 || opcion > 5)
+        {
+            cout << "Opcion no valida" << endl;
+            menuCierreCaja();
+        }
+
+        if (opcion == 5)
+        {
+            return;
+        }
+    }
+}
+
+void agregarCliente(Cliente clientes[], int &cantidadClientes)
+{
+    while (cantidadClientes < 100)
+    {
+        cout << "Ingrese el ID del cliente: " << endl;
+        cin >> clientes[cantidadClientes].clienteID;
+        cout << "Ingrese el nombre del cliente: " << endl;
+        cin >> clientes[cantidadClientes].nombre;
+        cout << "Ingrese el apellido del cliente: " << endl;
+        cin >> clientes[cantidadClientes].apellido;
+        cout << "Ingrese el email del cliente: " << endl;
+        cin >> clientes[cantidadClientes].email;
+
+        cantidadClientes++;
+
+        cout << "Cliente agregado exitosamente" << endl;
+        char continuar;
+        cout << "Desea agregar otro cliente? (s/n): ";
+        cin >> continuar;
+        if (continuar == 'n' || continuar == 'N')
+            return;
+    }
+    cout << "Se alcanzo el maximo de clientes (100)." << endl;
+}
+
+void listarCliente(Cliente clientes[], int cantidadClientes)
+{
+    if (cantidadClientes == 0)
+    {
+        cout << "No hay clientes para mostrar." << endl;
+        return;
+    }
+    cout << "----LISTA DE CLIENTES (" << cantidadClientes << ")----" << endl;
+    for (int i = 0; i < cantidadClientes; ++i)
+    {
+        cout << "ID: " << clientes[i].clienteID << endl;
+        cout << "Nombre: " << clientes[i].nombre << endl;
+        cout << "Apellido: " << clientes[i].apellido << endl;
+        cout << "Email: " << clientes[i].email << endl;
+        cout << "-----------------------------" << endl;
+    }
+}
+
+void inventario(Producto productos[], int cantidadProductos)
+{
+    productos[0] = {9901, "Teclado", 1000, 99};
+    productos[1] = {9902, "Mouse", 1500, 99};
+    productos[2] = {9903, "Monitor", 1000, 99};
+    productos[3] = {9904, "Notebook", 100000, 99};
+    productos[4] = {9905, "Webcam", 500, 99};
+    productos[5] = {9906, "Barra de sonido", 1100, 99};
+    productos[6] = {9907, "Estabilizador de tension", 100, 99};
+    productos[7] = {9908, "Soporte de monitor", 50, 99};
+    productos[8] = {9909, "Silla Gamer", 20000, 99};
+    productos[9] = {9010, "Mousepad", 20, 99};
+}
+
+void ingresarFactura(Factura facturas[], int &cantidadFacturas, Cliente clientes[], int cantidadClientes, Producto productos[], int cantidadProductos)
+{
+    char continuar = 's';
+    while (cantidadFacturas < 100 && (continuar == 's' || continuar == 'S'))
+    {
+        cout << "Ingrese el dia de la factura: " << endl;
+        cin >> facturas[cantidadFacturas].facturaDia;
+        cout << "Ingrese el mes de la factura: " << endl;
+        cin >> facturas[cantidadFacturas].facturaMes;
+        cout << "Ingrese el anio de la factura: " << endl;
+        cin >> facturas[cantidadFacturas].facturaAnio;
+        cout << "Ingrese el numero de la factura: " << endl;
+        cin >> facturas[cantidadFacturas].numeroFactura;
+        cout << "Ingrese el ID del cliente: " << endl;
+        for (int i = 0; i < cantidadClientes; ++i)
+        {
+            cout << "ID: " << clientes[i].clienteID << " - Nombre: " << clientes[i].nombre << " " << clientes[i].apellido << endl;
+        }
+        cin >> facturas[cantidadFacturas].clienteID;
+        // Quiero que el usuario pueda ver los skus disponibles, quiero que lo ingrese y que si no existe le diga que no existe, quiero que luego ingrese la cantidad y que el precio unitario se cargue solo desde el inventario
+        char continuarProducto = 's';
+        while (continuarProducto == 's' || continuarProducto == 'S')
+        {
+            // Mostrar productos disponibles A DESARROLLAR
+            cout << "Desea agregar otro producto a la factura? (s/n): ";
+            cin >> continuarProducto;
+        }
+        
+        cantidadFacturas++;
+        cout << "Factura ingresada exitosamente" << endl;
+        cout << "Desea agregar otra factura? (s/n): ";
+        cin >> continuar;
+    }
+}
+
+void listarFacturas(Factura facturas[], int cantidadFacturas, Cliente clientes[], int cantidadClientes, Producto productos[], int cantidadProductos)
+{
 }
